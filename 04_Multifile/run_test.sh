344b346d0285ef562e2ef8f49713770bfcfbaa18
davletov-aa <adavletov94@gmail.com>
2020-10-12 22:16:38 +0300

    some refactoring and debugging

---
+++ 04_Multifile/run_test.sh
@@ -0,0 +1,10 @@
+TEST_DIR=$1
+
+for x in prog prog-a prog-so;
+do
+	cp -f $(x) "run";
+	./run 2> "$TEST_DIR/$(x).txt";
+	./run "la" >> "$TEST_DIR/$(x).txt";
+	./run "la" "la" "la" >> "$TEST_DIR/$(x).txt";
+	rm "run"
+done
